a4bdf1601b7e99b937e10754ac3ce069b0b7de36
Andrew Myers <ajmyers5@gmail.com>
2020-05-28 16:55:50 -0500

    build

---
+++ build/precache-manifest.e60cbd03003df13322411f19c532643c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51c754663de797ae07afb58e73b69aed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44156a5faba3e1c46079",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "96fd1e1b15fa90edf368",
+    "url": "/static/js/2.6bf6bc8e.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.6bf6bc8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44156a5faba3e1c46079",
+    "url": "/static/js/main.2d0fc01e.chunk.js"
+  },
+  {
+    "revision": "504a74b7eaa1fe616c39",
+    "url": "/static/js/runtime-main.084c52ba.js"
+  }
+]);
